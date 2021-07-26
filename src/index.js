629f4362bb855df8db28bb1537ef44558e05ba64
joaquinmal <48302550+joaquinmal@users.noreply.github.com>
2021-07-26 16:54:10 -0500

    Execution Contexts, Hoisting, Scopes, and Closures

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import '../assets/css/style.css';
+
+// Execution Contexts, Hoisting, Scopes, and Closures
+console.log('name: ', name)
+console.log('handle: ', handle)
+console.log('getUser :', getUser)
+
+var name = 'Tyler'
+var handle = '@tylermcginnis'
+
+function getUser () {
+  return {
+    name: name,
+    handle: handle
+  }
+}
